01b1b14ad5c442f5f7cadaeb0b512780d9bf209d
Suric1 <igorbronko@gmail.com>
2022-08-31 19:25:15 +0300

    first lessons JS

---
+++ script/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 2;
+
+console.log(number);
+
+const obj = {
+  a: 50,
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+{
+  let result = 50;
+  console.log(result);
+}
+
+// alert(5);
+
+document.querySelector('.title').innerText = 'SURICAT HHI';
+
+document.querySelector('.subtitle').innerText = 'Surbi';
+
+document.querySelector('.descr').innerText = 52 + 63;
