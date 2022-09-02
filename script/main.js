ab3bdfc93667562ef4d08f292e5364de49658c34
Suric1 <igorbronko@gmail.com>
2022-09-03 00:06:35 +0300

    добавлены логические операторы

--- script/main.js
+++ script/main.js
@@ -40,3 +40,98 @@ console.log(isChecked || isChecked);
 console.log(isChecked || !isClose);
 console.log(2 + 2 * 2 != 8);
 console.log(2 + 2 * 2 !== 8);
+
+//  ЛОГИЧЕСКИЕ ОПЕРАТОРЫ
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//   console.log('Я сыт');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null & 5);
+// console.log(0 && 'fsafaa');
+
+// Оператор И (&&) Всегда возвращает первое ложное значение и дальше код не идет
+// Если все аргументы верны код дошел до конца то возвращается значение последнего аргумента
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//   console.log('Все сыты!');
+// } else {
+//   console.log('Мы уходим!');
+// }
+
+// const hamburger = 0;
+// const fries = 0;
+// const cola = 0;
+
+// console.log(hamburger || cola || fries);
+
+// if (hamburger || cola || fries) {
+//   console.log('Все довольны!');
+// } else {
+//   console.log('Мы уходим!');
+// }
+
+// Оператор ИЛИ ( || ) запинается на правдивом выражении. Когда правда найдена то значение будет возвращено
+// Если все не правда, возвращается последнее ложное значение
+
+let johnReport,
+  alexReport,
+  samReport,
+  mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+const hamburger = 3;
+const fries = 0;
+const cola = 2;
+const nuggets = 2;
+
+console.log((hamburger === 3 && cola === 2) || (fries === 3 && nuggets));
+
+if (hamburger || cola || fries) {
+  console.log('Все довольны!');
+} else {
+  console.log('Мы уходим!');
+}
+
+//  ОПЕРАТОР НЕ !
+console.log(!0);
+
+// ЗАДАЧИ 
+// console.log( NaN || 2 || undefined );
+
+// console.log( NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( !1 && 2 || !3 );
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+
+
