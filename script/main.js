389bc280a8c086c88df7976ca026ac94c88106fb
Suric1 <igorbronko@gmail.com>
2022-09-03 23:18:08 +0300

    вложенность циклов

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //  УРОК --ЦИКЛЫ-- while // do // for
-
+//  ИТЕРАЦИЯ ПОВТОРЕНИЯ ЦИКЛА
 let num = 50;
 
 //  ЦИКЛ while
@@ -20,10 +20,45 @@ do {
 
 //  Цикл for
 
-for (let i = 1; i < 16; i++) {
-  if (i === 6) {
-    // break;
-    continue;
+// for (let k = 1; k < 16; k++) {
+//   if (k === 6) {
+//     // break;
+//     continue;
+//   }
+//   console.log(k);
+// }
+
+// break прерывает цикл
+// continue позволяет нам пропустить шаг который нам не нужен  но при этом не прирывает полностью цикл
+
+//  ЦИКЛ В ЦИКЛЕ И МЕТКИ
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i);
+//   for (let j = 0; j < 3; j++) {
+//     console.log(j);
+//   }
+// }
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+  for (let j = 0; j < i; j++) {
+    result += '*';
   }
-  console.log(i);
+  result += '\n';
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First lvl ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second lvl ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) break first;
+      console.log(`Third lvl ${k}`);
+    }
+  }
 }
