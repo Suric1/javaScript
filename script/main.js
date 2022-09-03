fbecfef1c56cbd348b6ed9429764fa62725218c2
Suric1 <igorbronko@gmail.com>
2022-09-03 17:25:32 +0300

    if else else if Условия

--- script/main.js
+++ script/main.js
@@ -1,48 +1,43 @@
-// Уроки по Циклам и Условиям
-
 'use strict';
 
-// УСЛОВИЯ
+// УРОК -- УСЛОВИЯ   if // else//  else if
 
-// if (4 == 9) {
-//   console.log('Ok');
-// } else {
-//   console.log('Error');
-// }
+if (4 == 9) {
+  console.log('Ok');
+} else {
+  console.log('ERROR');
+}
 
-// const num = 50;
-
-// if (num < 49) {
-//   console.log('Error');
-// } else if (num > 100) {
-//   console.log('МНОГО');
-// } else {
-//   console.log('ОК');
-// }
-
-// ДРУГОЙ СПОСОБ ЗАПИСИ УСЛОВИЯ (ТЕРНАРНОЕ)
-
-// const num = 50;
-
-//1 аргумент  2 аргумент            3 аргумент
-// num === 50 ? console.log('OK~') : console.log('EROR');
-
-//  МОДИФИКАЦИЯ IF  / ELSE  / IF ELSE
-
-//  switch всегда строгое сравнение
 const num = 50;
 
-switch (num) {
-  case 49:
-    console.log('Неверно');
+if (num < 49) {
+  console.log('ERROR');
+} else if (num > 100) {
+  console.log('МНОГО');
+} else {
+  console.log('ОК!');
+}
+
+//  ТЕРНАРНЫЙ ОПЕРАТОР
+// В ЕГО РАБОТЕ УЧАСТВУЮТ 3 АРГУМЕНТА (1 АРГУМЕНТ : num === 50)(2 АРГУМЕНТ console.log('ОК!') (3 АРГУМЕНТ console.log('ERROR')
+
+num === 50 ? console.log('ОК!') : console.log('ERROR');
+
+const number = 40;
+// switch ВСЕГДА ИДЕТ НА СТРОГОЕ СООТВЕТСВИЕ
+
+switch (number) {
+  case 41:
+    console.log('НЕВЕРНО');
     break;
   case 100:
-    console.log('Неверно');
+    console.log('НЕВЕРНО');
     break;
-  case 50:
-    console.log('Верно');
+  case 40:
+    console.log('ВЕРНО');
     break;
   default:
-    console.log('Не в этот раз');
+    console.log('НЕ в этот раз');
     break;
 }
+// ЕСЛИ ВСЕ НЕВЕРНО и ДЛЯ ТОГО ЧТОБЫ ВЫПОЛНИТЬ КАКОЕ ТО ДЕЙСТВИЕ ПО УМОЛЧАНИЮ default
