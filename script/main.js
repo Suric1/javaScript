00cec5752a23d2aed551f484e9ed5fa9f9e31e24
Suric1 <igorbronko@gmail.com>
2022-09-05 22:17:35 +0300

    Функция как аргумент

--- script/main.js
+++ script/main.js
@@ -80,3 +80,67 @@ function blockWidth() {
   w++;
   document.querySelector('.out-1').style.width = w + 'px';
 }
+
+//  АРГУМЕНТЫ ФУНКЦИИ
+
+function f1(a, b, c) {
+  console.log(a + b + c);
+}
+
+f1(44, 55, 11);
+f1(2, 3, 4, 5, 6);
+f1(5, 1);
+
+function showSum(elem, x, y) {
+  document.querySelector(elem).textContent = x + y;
+}
+function showSum2(elem, x, y) {
+  elem.textContent = x + y;
+}
+
+showSum('.out-2', 5, 6);
+const out1 = document.querySelector('.out-1');
+showSum2(out1, 5, 6);
+
+function showSum3(x, y, elem = '.out-1') {
+  console.log(arguments);
+  document.querySelector(elem).textContent = x + y;
+}
+
+showSum3(10, 52, '.out-2');
+
+function showSumAll() {
+  console.log(arguments);
+  // let sum = 0;
+  // for (let i = 0; i < arguments.length; i++) {
+  //   sum += arguments[i];
+  // }
+  let sum = Array.from(arguments).reduce((accum, item) => (accum += item));
+  console.log(sum);
+}
+
+showSumAll(4, 5, 6, 1000);
+
+function showSumAll2(...args) {
+  console.log(args);
+  // let sum = 0;
+  // for (let i = 0; i < arguments.length; i++) {
+  //   sum += arguments[i];
+  // }
+  let sum = args.reduce((accum, item) => (accum += item));
+  console.log(sum);
+}
+
+showSumAll(4, 5, 6, 1000);
+
+//  функция как аргумент
+
+function showAll(result) {
+  document.querySelector('.out-1').innerHTML = `<b>${result}</b>`;
+}
+
+function showSumAll3(num, drawFunction) {
+  drawFunction(num);
+}
+
+showSumAll3(100000000000, showAll);
