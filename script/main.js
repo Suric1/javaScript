1e1855c909e4714826a2bd953ede5a67fc44800e
Suric1 <igorbronko@gmail.com>
2022-09-13 23:15:11 +0300

    Добавлены новые функции

--- script/main.js
+++ script/main.js
@@ -1,87 +1,130 @@
 'use strict';
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms
 
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  
+  while (numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
 
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt('Один из последних просмотренных фильмов', ''),
-//     b = prompt('На сколько оцените его?', '');
 
-//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log('all done');
-//   } else {
-//     console.log('error');
-//     i--;
-//   }
-// }
+start()
 
-// if (personalMovieDB.count < 10) {
-//   console.log('Просмотрено довольно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-//   console.log('Вы классический зритель');
-// } else if (personalMovieDB.count >= 30) {
-//   console.log('Вы Киноман');
-// } else {
-//   console.log('Произошла ошибка');
-// }
-// console.log(personalMovieDB);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-//  КАЛЬКУЛЯТОР СЛОЖЕНИЯ
 
-let numberOne = document.querySelector('.number-one');
-let numberTwo = document.querySelector('.number-two');
 
-let plusButton = document.querySelector('.plus');
-let minusButton = document.querySelector('.minus');
-let divideButton = document.querySelector('.divide');
-let multiplyButton = document.querySelector('.multiply');
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('all done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
+}
 
-let clearButton = document.querySelector('.clear');
+rememberMyFilms()
 
-let resultText = document.querySelector('.result');
 
-clearButton.addEventListener('click', function () {
-  resultText.innerText = '';
-  numberOne.value = '';
-  numberTwo.value = '';
-});
-plusButton.addEventListener('click', function () {
-  let numOne = parseInt(numberOne.value);
-  let numTwo = parseInt(numberTwo.value);
+console.log(personalMovieDB);
 
-  let sum = numOne + numTwo;
-  resultText.innerText = sum;
-});
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы Киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
+}
+detectPersonalLevel()
 
-//  КАЛЬКУЛЯТОР ВЫЧИТАНИЯ
-minusButton.addEventListener('click', function () {
-  let numOne = parseInt(numberOne.value);
-  let numTwo = parseInt(numberTwo.value);
 
-  let minus = numOne - numTwo;
-  resultText.innerText = minus;
-});
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
 
-divideButton.addEventListener('click', function () {
-  let numOne = parseInt(numberOne.value);
-  let numTwo = parseInt(numberTwo.value);
+showMyDB(personalMovieDB.privat)
 
-  let divide = numOne / numTwo;
-  resultText.innerText = divide;
-});
 
-multiplyButton.addEventListener('click', function () {
-  let numOne = parseInt(numberOne.value);
-  let numTwo = parseInt(numberTwo.value);
+function writeYourGenres() {
+  for ( let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`)
+  }
+}
 
-  let multiply = numOne * numTwo;
-  resultText.innerText = multiply;
-});
+writeYourGenres()
+
+
+
+//
+// //  КАЛЬКУЛЯТОР СЛОЖЕНИЯ
+//
+// let numberOne = document.querySelector('.number-one');
+// let numberTwo = document.querySelector('.number-two');
+//
+// let plusButton = document.querySelector('.plus');
+// let minusButton = document.querySelector('.minus');
+// let divideButton = document.querySelector('.divide');
+// let multiplyButton = document.querySelector('.multiply');
+//
+// let clearButton = document.querySelector('.clear');
+//
+// let resultText = document.querySelector('.result');
+//
+// clearButton.addEventListener('click', function () {
+//   resultText.innerText = '';
+//   numberOne.value = '';
+//   numberTwo.value = '';
+// });
+// plusButton.addEventListener('click', function () {
+//   let numOne = parseInt(numberOne.value);
+//   let numTwo = parseInt(numberTwo.value);
+//
+//   let sum = numOne + numTwo;
+//   resultText.innerText = sum;
+// });
+//
+// //  КАЛЬКУЛЯТОР ВЫЧИТАНИЯ
+// minusButton.addEventListener('click', function () {
+//   let numOne = parseInt(numberOne.value);
+//   let numTwo = parseInt(numberTwo.value);
+//
+//   let minus = numOne - numTwo;
+//   resultText.innerText = minus;
+// });
+//
+// divideButton.addEventListener('click', function () {
+//   let numOne = parseInt(numberOne.value);
+//   let numTwo = parseInt(numberTwo.value);
+//
+//   let divide = numOne / numTwo;
+//   resultText.innerText = divide;
+// });
+//
+// multiplyButton.addEventListener('click', function () {
+//   let numOne = parseInt(numberOne.value);
+//   let numTwo = parseInt(numberTwo.value);
+//
+//   let multiply = numOne * numTwo;
+//   resultText.innerText = multiply;
+// });
