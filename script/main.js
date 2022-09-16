5a03a49ba5bc4bb7b4838ea788d313a1f564361d
Suric1 <igorbronko@gmail.com>
2022-09-16 22:06:38 +0300

    улучшение функций

--- script/main.js
+++ script/main.js
@@ -25,10 +25,10 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов', ''),
+    const a = prompt('Один из последних просмотренных фильмов', '').trim(),
       b = prompt('На сколько оцените его?', '');
     
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
       personalMovieDB.movies[a] = b;
       console.log('all done');
     } else {
@@ -128,3 +128,5 @@ writeYourGenres()
 //   let multiply = numOne * numTwo;
 //   resultText.innerText = multiply;
 // });
+
+
